a131aa0a66f927cacb778452cbc8d7e5c6ce1eec
Beegiees <116861215+Beegiees@users.noreply.github.com>
2022-10-30 20:28:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -171,8 +171,8 @@ app.get("/getNewID", function (req, res) {
 	}
 });
 
-app.listen(8080, () => {
-	console.log("Application started and Listening on port 8080");
+app.listen(80, () => {
+	console.log("Application started and Listening on port 80");
 });
 
 module.exports = app;
